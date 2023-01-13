dcdcbf1fb8a1910137e120c07697fa962758c350
SamySam2099 <38728942+SamySam2099@users.noreply.github.com>
2023-01-13 14:45:29 +0000

    Added changes from react router lesson

--- src/components/category-list/CategoryList.jsx
+++ src/components/category-list/CategoryList.jsx
@@ -3,13 +3,13 @@ import './category-list.scss'
 import CategoryItem from '../category-item/CategoryItem'
 
 const CategoryList = ({ categories }) => {
-	return (
-		<div className="categories-container">
-			{categories.map((category) => (
-				<CategoryItem key={category.id} category={category} />
-			))}
-		</div>
-	)
+  return (
+    <div className='categories-container'>
+      {categories.map(category => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  )
 }
 
 export default CategoryList
